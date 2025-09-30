cd6a81d85e752e9ac28fd7c91f993936df1183b5
Мунара Султан кызы <munarasultankyzy777@gmail.com>
2025-09-30 21:38:25 +0600

    errors устранены

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / 'static']  # Для разработки
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # Для продакшена
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
